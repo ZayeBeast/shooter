9ac01d647da3c3f0a2e234710a62c7d60ba491dc
GummyBear <dawid1siekanski@gmail.com>
2019-01-09 20:52:43 +0100

    tank constructor and canvas added

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function Tank (id){
+	this.img="tank.jpg";	
+}
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const image = new Image();
+image.addEventListener('load',function(){ctx.drawImage(image,0,0)});
+image.src="tank.jpg";
 let key;
 document.addEventListener('keydown',move,false);
 function move(event){
