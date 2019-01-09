aa17caa56b34271712cc055f63700ec8b1b73f0c
GummyBear <dawid1siekanski@gmail.com>
2019-01-09 21:17:28 +0100

    render function added

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-function Tank (id){
-	this.img="tank.jpg";	
-}
-
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const image = new Image();
-image.addEventListener('load',function(){ctx.drawImage(image,0,0)});
-image.src="tank.jpg";
+
+function Tank (id){
+	this.render=function(){
+		const image = new Image();
+		image.src="tank.jpg";
+		image.addEventListener('load',function(){ctx.drawImage(image,0,0)});
+	}	
+}
+
+
+const tank1 = new Tank(1);
 let key;
 document.addEventListener('keydown',move,false);
 function move(event){
