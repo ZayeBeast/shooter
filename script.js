5b7634f57b37d27c5ef142c8de19536465cf1bd1
GummyBear <dawid1siekanski@gmail.com>
2019-01-10 20:32:00 +0100

    loop game and background added

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const canvw=1000;
+const canvh=500;
+let key;
 
-function Tank (id){
-	this.render=function(){
-		const image = new Image();
-		image.src="tank.jpg";
-		image.addEventListener('load',function(){ctx.drawImage(image,0,0)});
+canvas.width=canvw;
+canvas.height=canvh;
+
+class Tank {
+	constructor(id) {
+		this.id=id;	
+		this.image = new Image();
+		this.image.src='tank'+id+'.jpg';
+	}	
+	
+	render(){
+		ctx.drawImage(this.image,0,0);
 	}	
 }
 
+function bg(){
+	ctx.fillStyle='blue';
+	ctx.fillRect(0,0,canvw,canvh);
+}
 
-const tank1 = new Tank(1);
-tank1.render();
-let key;
+const tank1 = new Tank(1,);
+
 document.addEventListener('keydown',move,false);
 function move(event){
 key = event.key; 
 }
+
+function game(){
+	bg();
+	tank1.render();
+}
+
+setInterval(game,25);
