903fdbaccd545c72b81c0aefeecad16b90a41e12
Bialaz <igor.bialas@gmail.com>
2019-01-12 18:44:57 +0100

    Tabs and comments repaired

--- script.js
+++ script.js
@@ -16,45 +16,45 @@ class Tank {
 		document.addEventListener('keydown', evt=>this.move(evt), false);
 	}
 	render(){
-    var self = this;
-    document.addEventListener('click',evt=> self.handleCellClick.call(self, evt) , false);
-    ctx.translate(this.coordx, this.coordy);
-    ctx.rotate(Math.PI / 180* this.angle);
-    ctx.drawImage(this.image,this.coordx,this.coordy, 50, 50);
-    this.angle=0;
-    console.log("render");
+    		var self = this;
+    		document.addEventListener('click',evt=> self.handleCellClick.call(self, evt) , false);
+    		ctx.translate(this.coordx, this.coordy);
+    		ctx.rotate(Math.PI / 180* this.angle);
+    		ctx.drawImage(this.image,this.coordx,this.coordy, 50, 50);
+    		this.angle=0;
+    		console.log("render");
 		
 	}
 	
-		move(event){
-      console.log(this.coordx +" | "+this.coordy);
-      const spd = 2;                                // Speed's value
-          switch (event.keyCode) {
-              case 37:                              // Left
-                ctx.fillRect(0,0,canvw,canvh);
-                this.angle-=5;
-                break;
+	move(event){
+      		console.log(this.coordx +" | "+this.coordy);
+      		const spd = 2;                                // Speed's value
+          	switch (event.keyCode) {
+              		case 37:                              // Left
+               			ctx.fillRect(0,0,canvw,canvh);
+               			this.angle-=5;
+                		break;
      
-              case 40:                              //Up
-                ctx.fillRect(0,0,canvw,canvh);
-		        if(this.coordy<2){
-                	this.coordy+=spd;
-		        }
-                break;
+              		case 40:                              //Up
+                		ctx.fillRect(0,0,canvw,canvh);
+		        	if(this.coordy<2){
+                			this.coordy+=spd;
+		        	}
+                		break;
      
-              case 39:                              // Right
-                ctx.fillRect(0,0,canvw,canvh);
-                this.angle+=5;
-                break;
+              		case 39:                              // Right
+                		ctx.fillRect(0,0,canvw,canvh);
+                		this.angle+=5;
+                		break;
      
-              case 38:                              // Down
-                ctx.fillRect(0,0,canvw,canvh);
-		        if(this.coordy>-2){
-                	this.coordy-=spd;
-		        }
-                break;
-      }	
-  }
+              		case 38:                              // Down
+                		ctx.fillRect(0,0,canvw,canvh);
+		        	if(this.coordy>-2){
+                			this.coordy-=spd;
+		        	}
+                		break;
+    		}	
+	}
 }
 
 function bg(){                     // (sth like) refreshing, it shows background.  
@@ -63,15 +63,10 @@ function bg(){                     // (sth like) refreshing, it shows background
 }
 
 const tank1 = new Tank(1);        // creating new object 
-tank1.coordx = 0;
-tank1.coordy = 0;
-
-
 
 function game(){
 	bg();
-	tank1.render();
-	
+	tank1.render();	
 }
 
-setInterval(game,25); // It does game() func every 25 milisec.
+setInterval(game,25); // It does game() func 40 times per minute.
