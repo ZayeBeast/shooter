9b512302e1114673395d9dd448b69eb1ad557b82
GummyBear <dawid1siekanski@gmail.com>
2019-01-13 21:33:34 +0100

    tank rotates from its middle, but there are 'smudges'

--- script.js
+++ script.js
@@ -9,24 +9,25 @@ class Tank {
 	constructor(id) {
 		this.id=id;	
 		this.image = new Image();
-		this.image.src='tank1.svg';
+		this.image.src='tank'+id+'.svg';
 		this.angle=0;
 		this.coordx=0;
 		this.coordy=0;
+		
 		document.addEventListener('keydown', evt=>this.move(evt), false);
 	}
 	render(){
     		var self = this;
-    		ctx.translate(this.coordx, this.coordy);
+    		ctx.translate(this.coordx+25,this.coordy+25);
     		ctx.rotate(Math.PI / 180* this.angle);
-    		ctx.drawImage(this.image,this.coordx,this.coordy, 50, 50);
-    		this.angle=0;
-    		console.log("render");
-		
+			ctx.translate(-(this.coordx+25),-(this.coordy+25));
+			ctx.drawImage(this.image,this.coordx,this.coordy,50,50);
+    		//console.log("render");
+			console.log(this.coordx +" | "+this.coordy);
+			this.angle=0;
 	}
 	
 	move(event){
-      		console.log(this.coordx +" | "+this.coordy);
       		const spd = 2;                                // Speed's value
       		let key = event.key;
       		if(key.substr(0,5)=="Arrow")
@@ -37,9 +38,7 @@ class Tank {
                 		break;
      
               		case 'Down':
-		        	if(this.coordy<2){
                 			this.coordy+=spd;
-		        	}
                 		break;
      
               		case 'Right':
@@ -47,9 +46,7 @@ class Tank {
                 		break;
      
               		case 'Up':
-		        	if(this.coordy>-2){
                 			this.coordy-=spd;
-		        	}
                 		break;
     		}	
 	}
